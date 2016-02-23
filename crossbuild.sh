cbbc1784227f40dfaed5484b2dcee8862f423c26
obijuan <juan@iearobotics.com>
2016-02-23 15:50:13 +0100

    Compilation of icetime OK

--- crossbuild.sh
+++ crossbuild.sh
@@ -20,7 +20,7 @@ sudo apt-get install cmake  git-core mingw-w64 mingw-w64-tools zip
 git -C icestorm pull || git clone https://github.com/cliffordwolf/icestorm.git
 cd icestorm
 
-##-------- Build iceprog
+## -------- Build iceprog
 cd iceprog
 cp $WORK/packages/windows/Makefile.iceprog Makefile
 make -j$(( $(nproc) -1))
@@ -29,3 +29,10 @@ mkdir -p $WORK/windist
 mkdir -p $WORK/windist/$NAME
 mkdir -p $WORK/windist/$NAME/bin
 cp iceprog.exe $WORK/windist/$NAME/bin
+
+
+# ----------- Building icetime
+cd ../icetime
+cp $WORK/packages/windows/Makefile.icetime Makefile
+make -j$(( $(nproc) -1))
+cp icetime.exe $WORK/windist/$NAME/bin
