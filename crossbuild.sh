6b32d65be1a8e906d029acfdeef6dca886683f3d
obijuan <juan@iearobotics.com>
2016-02-23 15:06:49 +0100

    Iceprog cross compilation ok

---
+++ crossbuild.sh
@@ -0,0 +1,31 @@
+################################################
+# icestorm builder for different architectures #
+################################################
+
+NAME=toolchain-icestorm
+ARCH=windows
+VERSION=1
+$TCDIR=$PWD/windist/$NAME
+
+# Store current dir
+WORK=$PWD
+
+# Enter into the code directory
+mkdir -p winbuild; cd winbuild
+
+# Install dependencies
+sudo apt-get install cmake  git-core mingw-w64 mingw-w64-tools zip
+
+# download icestorm
+git -C icestorm pull || git clone https://github.com/cliffordwolf/icestorm.git
+cd icestorm
+
+##-------- Build iceprog
+cd iceprog
+cp $WORK/packages/windows/Makefile.iceprog Makefile
+make -j$(( $(nproc) -1))
+
+mkdir -p $WORK/windist
+mkdir -p $WORK/windist/$NAME
+mkdir -p $WORK/windist/$NAME/bin
+cp iceprog.exe $WORK/windist/$NAME/bin
