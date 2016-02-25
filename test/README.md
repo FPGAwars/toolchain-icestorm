95c4106d1d8e6e2a8833e2e3154b0bdbe7b24589
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-02-25 13:35:33 +0100

    Add test example script

---
+++ test/README.md
@@ -0,0 +1,21 @@
+#test
+
+```bash
+$ bash test_iceprog.sh iceprog
+Testing iceprog file
+------------------
+- 1. File exists
+- 2. File is executable
+- 3. File is static
+------------------
+All tests [PASSED]
+```
+
+```bash
+$ bash test_iceprog.sh iceprogx
+Testing iceprogx file
+------------------
+- 1. File exists
+- 2. File is executable
+- 3. File is static [FAILED]
+```
---
+++ test/test_iceprog.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+FILE=$1
+
+echo "Testing $FILE file" >&2
+echo "------------------" >&2
+
+function test0 {
+    if "${@:2}"
+    then
+        echo "$1" >&2
+    else
+        echo "$1 [FAILED]" >&2
+        exit 1
+    fi
+}
+
+function test1 {
+    test0 "- 1. File exists" test -e $1
+}
+
+function test2 {
+    test0 "- 2. File is executable" test -x $1
+}
+
+function test3 {
+    output=$(ldd $1 | grep "not a dynamic executable")
+    test0 "- 3. File is static" test -n "$output"
+}
+
+test1 $FILE
+test2 $FILE
+test3 $FILE
+
+echo "------------------" >&2
+echo "All tests [PASSED]" >&2
