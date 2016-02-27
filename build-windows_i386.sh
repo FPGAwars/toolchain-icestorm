26ab6a0e1a2a70ce5ca5d4b97a621d6351912e1f
obijuan <juan@iearobotics.com>
2016-02-27 12:00:13 +0100

    Script for cross-compiling for windows 32 bits. Initial version. Only for iceprog

---
+++ build-windows_i386.sh
@@ -0,0 +1,95 @@
+#####################################################
+# Icestorm toolchain builder for Windows 32 bits    #
+# It should be execute on a Linux x86_64 machine    #
+#####################################################
+
+# Generate toolchain-icestorm.tar.gz from source code
+# sources: http://www.clifford.at/icestorm/
+# This tarball can be unpacked in ~/.platformio/packages
+
+# -- Upstream folder. This is where all the toolchain is stored
+# -- from the github
+UPSTREAM=upstream
+
+# -- Git url were to retieve the upstream sources
+GIT_ICESTORM=https://github.com/cliffordwolf/icestorm.git
+
+# -- Folder for storing the generated packages
+PACK_DIR=packages
+
+# -- Target architecture
+ARCH=windows_i386
+
+# -- Directory for compiling the tools
+BUILD_DIR=build_$ARCH
+
+# -- Icestorm directory
+ICESTORM=icestorm
+
+# -- Iceprog directory
+ICEPROG=iceprog
+
+# --- Directory where the files for patching the upstream are located
+DATA=build-data/$ARCH
+
+# -- toolchain name
+NAME=toolchain-icestorm
+
+# -- Directory for installation the target files
+INSTALL=$PWD/$BUILD_DIR/$NAME
+
+VERSION=6
+PACKNAME=$NAME-$ARCH-$VERSION
+TARBALL=$PACKNAME.tar.gz
+
+# Store current dir
+WORK=$PWD
+
+# Install dependencies
+echo "Installing dependencies..."
+sudo apt-get install build-essential clang bison flex libreadline-dev \
+                     gawk tcl-dev libffi-dev git mercurial graphviz   \
+                     xdot pkg-config python python3 libftdi-dev git-core \
+                     mingw-w64 mingw-w64-tools zip
+
+# Create the upstream directory and enter into it
+mkdir -p $UPSTREAM
+cd $WORK/$UPSTREAM
+
+# -------- Clone the toolchain from the github
+# -- Icetools
+git -C $ICESTORM pull || git clone $GIT_ICESTORM $ICESTORM
+
+# -------- Create the packages directory
+cd $WORK
+mkdir -p $PACK_DIR
+
+# -------- Create the build dir and enter into it
+mkdir -p $BUILD_DIR
+cd $WORK/$BUILD_DIR
+
+# --- Create the target folder
+mkdir -p $NAME
+mkdir -p $NAME/bin
+
+# ---- Copy the upstream sources into the build directory
+cp -r $WORK/$UPSTREAM/$ICESTORM/$ICEPROG .
+
+# --------- Compile the iceprog
+cd $WORK/$BUILD_DIR/$ICEPROG
+
+# -- Apply the patches
+cp $WORK/$DATA/Makefile.iceprog $WORK/$BUILD_DIR/$ICEPROG/Makefile
+
+# -- Compile it!
+make
+
+# -- Copy the executable to the bin dir
+cp iceprog.exe $INSTALL/bin
+
+# -- Create the package
+cd $WORK/$BUILD_DIR
+tar vzcf $TARBALL $NAME
+
+# -- Move the package to the packages dir
+mv $TARBALL $WORK/$PACK_DIR
