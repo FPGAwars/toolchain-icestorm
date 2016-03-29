88d9376f94904e0d605026741912ecd8a346eb95
obijuan <juan@iearobotics.com>
2016-03-29 16:53:13 +0200

    Windows_i386: Arachne: now the chip-xk bin files are read ok. It has been applied the patch of the pull-request #29 in the arachne github, that is pending to merge

--- build-data/windows_i386/arachne.patch/src/arachne-pnr.cc
+++ build-data/windows_i386/arachne.patch/src/arachne-pnr.cc
@@ -1,15 +1,15 @@
 /* Copyright (C) 2015 Cotton Seed
-
+   
    This file is part of arachne-pnr.  Arachne-pnr is free software;
    you can redistribute it and/or modify it under the terms of the GNU
    General Public License version 2 as published by the Free Software
    Foundation.
-
+   
    This program is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
    along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
@@ -39,13 +39,13 @@ const char *program_name;
 class null_streambuf : public std::streambuf
 {
 public:
-  int overflow(int c) override { return c; }
+  int overflow(int c) override { return c; }  
 };
 
 void
 usage()
 {
-  std::cout
+  std::cout 
     << "Usage:\n"
     << "\n"
     << "  " << program_name << " [options] [input-file]\n"
@@ -119,7 +119,7 @@ int
 main(int argc, const char **argv)
 {
   program_name = argv[0];
-
+  
   bool help = false,
     quiet = false,
     do_promote_globals = true,
@@ -137,7 +137,7 @@ main(int argc, const char **argv)
     *output_file = nullptr,
     *seed_str = nullptr,
     *binary_chipdb = nullptr;
-
+  
   for (int i = 1; i < argc; ++i)
     {
       if (argv[i][0] == '-')
@@ -153,7 +153,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               device = argv[i];
             }
@@ -162,7 +162,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               chipdb_file = argv[i];
             }
@@ -170,7 +170,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               binary_chipdb = argv[i];
             }
@@ -182,7 +182,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               pack_blif = argv[i];
             }
@@ -191,7 +191,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               pack_verilog = argv[i];
             }
@@ -199,7 +199,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               place_blif = argv[i];
             }
@@ -210,7 +210,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               pcf_file = argv[i];
             }
@@ -219,7 +219,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               package_name_cp = argv[i];
             }
@@ -230,7 +230,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               post_place_pcf = argv[i];
             }
@@ -239,7 +239,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               seed_str = argv[i];
             }
@@ -248,7 +248,7 @@ main(int argc, const char **argv)
             {
               if (i + 1 >= argc)
                 fatal(fmt(argv[i] << ": expected argument"));
-
+              
               ++i;
               output_file = argv[i];
             }
@@ -269,17 +269,17 @@ main(int argc, const char **argv)
             input_file = argv[i];
         }
     }
-
+  
   if (help)
     {
       usage();
       exit(EXIT_SUCCESS);
     }
-
+  
   if (device != "1k"
       && device != "8k")
     fatal(fmt("unknown device: " << device));
-
+  
   std::string package_name;
   if (package_name_cp)
     package_name = package_name_cp;
@@ -290,35 +290,35 @@ main(int argc, const char **argv)
       assert(device == "8k");
       package_name = "ct256";
     }
-
+  
   std::ostream *null_ostream = nullptr;
   if (quiet)
     logs = null_ostream = new std::ostream(new null_streambuf);
   else
     logs = &std::cerr;
-
+  
   unsigned seed = 0;
   if (seed_str)
     {
       std::string seed_s = seed_str;
-
+      
       if (seed_s.empty())
         fatal("invalid empty seed");
-
+      
       for (char ch : seed_s)
         {
           if (ch >= '0'
               && ch <= '9')
             seed = seed * 10 + (unsigned)(ch - '0');
           else
-            fatal(fmt("invalid character `"
+            fatal(fmt("invalid character `" 
                       << ch
                       << "' in unsigned integer literal in seed"));
         }
     }
   else
     seed = 1;
-
+  
   if (randomize_seed)
     {
       std::random_device rd;
@@ -326,50 +326,50 @@ main(int argc, const char **argv)
         seed = rd();
       } while (seed == 0);
     }
-
+  
   *logs << "seed: " << seed << "\n";
   if (!seed)
     fatal("zero seed");
-
+  
   random_generator rg(seed);
-
+  
   *logs << "device: " << device << "\n";
   std::string chipdb_file_s;
   if (chipdb_file)
     chipdb_file_s = chipdb_file;
   else
     chipdb_file_s = (std::string("+/share/arachne-pnr/chipdb-")
-                     + device
-                     + ".txt");
+                     + device 
+                     + ".bin");
   *logs << "read_chipdb " << chipdb_file_s << "...\n";
   const ChipDB *chipdb = read_chipdb(chipdb_file_s);
-
+  
   if (binary_chipdb)
     {
       *logs << "write_binary_chipdb " << binary_chipdb << "\n";
-
+      
       std::string expanded = expand_filename(binary_chipdb);
-      std::ofstream ofs(expanded);
+      std::ofstream ofs(expanded, std::ofstream::out | std::ofstream::binary);
       if (ofs.fail())
         fatal(fmt("write_binary_chidpb: failed to open `" << expanded << "': "
                   << strerror(errno)));
       obstream obs(ofs);
       chipdb->bwrite(obs);
-
+      
       // clean up
       if (chipdb)
         delete chipdb;
-
+      
       logs = nullptr;
       if (null_ostream)
         {
           delete null_ostream;
           null_ostream = nullptr;
         }
-
+      
       return 0;
     }
-
+  
   *logs << "  supported packages: ";
   bool first = true;
   for (const auto &p : chipdb->packages)
@@ -381,22 +381,22 @@ main(int argc, const char **argv)
       *logs << p.first;
     }
   *logs << "\n";
-
+  
   // chipdb->dump(std::cout);
-
+  
   auto package_i = chipdb->packages.find(package_name);
   if (package_i == chipdb->packages.end())
     fatal(fmt("unknown package `" << package_name << "'"));
   const Package &package = package_i->second;
-
+  
 #ifdef __AFL_HAVE_MANUAL_CONTROL
   __AFL_INIT();
 #endif
-
+  
   /*
   while (__AFL_LOOP(1000)) {
   */
-
+  
   Design *d;
   if (input_file)
     {
@@ -409,17 +409,17 @@ main(int argc, const char **argv)
       d = read_blif("<stdin>", std::cin);
     }
   // d->dump();
-
+  
   *logs << "prune...\n";
   d->prune();
 #ifndef NDEBUG
   d->check();
 #endif
   // d->dump();
-
+  
   {
     DesignState ds(chipdb, package, d);
-
+    
     if (route_only)
       {
         for (Instance *inst : ds.top->instances())
@@ -438,21 +438,21 @@ main(int argc, const char **argv)
             *logs << "read_pcf " << pcf_file << "...\n";
             read_pcf(pcf_file, ds);
           }
-
+        
         *logs << "instantiate_io...\n";
         instantiate_io(d);
 #ifndef NDEBUG
         d->check();
 #endif
         // d->dump();
-
+        
         *logs << "pack...\n";
         pack(ds);
 #ifndef NDEBUG
         d->check();
 #endif
         // d->dump();
-
+        
         if (pack_blif)
           {
             *logs << "write_blif " << pack_blif << "\n";
@@ -475,28 +475,28 @@ main(int argc, const char **argv)
             fs << "/* " << version_str << " */\n";
             d->write_verilog(fs);
           }
-
+        
         *logs << "place_constraints...\n";
         place_constraints(ds);
 #ifndef NDEBUG
         d->check();
 #endif
-
+        
         // d->dump();
-
+        
         *logs << "promote_globals...\n";
         promote_globals(ds, do_promote_globals);
 #ifndef NDEBUG
         d->check();
 #endif
         // d->dump();
-
+        
         *logs << "realize_constants...\n";
         realize_constants(chipdb, d);
 #ifndef NDEBUG
         d->check();
 #endif
-
+	
         *logs << "place...\n";
         // d->dump();
         place(rg, ds);
@@ -504,7 +504,7 @@ main(int argc, const char **argv)
         d->check();
 #endif
         // d->dump();
-
+        
         if (post_place_pcf)
           {
             *logs << "write_pcf " << post_place_pcf << "...\n";
@@ -525,12 +525,12 @@ main(int argc, const char **argv)
                                       ->connection_other_port());
                     assert(isa<Model>(top_port->node())
                            && cast<Model>(top_port->node()) == ds.top);
-
+                    
                     fs << "set_io " << top_port->name() << " " << pin << "\n";
                   }
               }
           }
-
+        
         if (place_blif)
           {
             for (const auto &p : ds.placement)
@@ -544,7 +544,7 @@ main(int argc, const char **argv)
                                       << "," << chipdb->tile_y(t)
                                       << "/" << pos));
               }
-
+            
             *logs << "write_blif " << place_blif << "\n";
             std::string expanded = expand_filename(place_blif);
             std::ofstream fs(expanded);
@@ -555,15 +555,15 @@ main(int argc, const char **argv)
             d->write_blif(fs);
           }
       }
-
+    
     // d->dump();
-
+    
     *logs << "route...\n";
     route(ds);
 #ifndef NDEBUG
     d->check();
 #endif
-
+    
     if (output_file)
       {
         *logs << "write_txt " << output_file << "...\n";
@@ -580,23 +580,23 @@ main(int argc, const char **argv)
         ds.conf.write_txt(std::cout, chipdb, d, ds.placement, ds.cnet_net);
       }
   }
-
+  
   if (d)
     delete d;
-
+  
   /*
   }
   */
-
+  
   if (chipdb)
     delete chipdb;
-
+  
   logs = nullptr;
   if (null_ostream)
     {
       delete null_ostream;
       null_ostream = nullptr;
     }
-
+  
   return 0;
 }
