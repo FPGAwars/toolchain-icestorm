dcaf76280ec5bd99dde0042809384de1afbb13ec
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2017-02-21 12:18:34 +0100

    Optimize Yosys & Yosys-abc size

--- scripts/compile_yosys.sh
+++ scripts/compile_yosys.sh
@@ -26,17 +26,18 @@ if [ $ARCH == "windows" ]; then
   make config-msys2
   sed -i "s/LIBS=\"lib\/x86\/pthreadVC2.lib -s\" ABC_USE_NO_READLINE=0/LIBS=\"-static -lm\" ABC_USE_NO_READLINE=1 ABC_USE_NO_PTHREADS=1/;" Makefile
   make -j$J ENABLE_TCL=0 ENABLE_PLUGINS=0 ENABLE_READLINE=0 ENABLE_COVER=0 \
-            YOSYS_VER_STR="Yosys 0.7 (Apio build)" \
+            YOSYS_VER="0.7 (Apio build)" \
             LDLIBS="-static -lstdc++ -lm"
 else
   make config-gcc
+  sed -i "s/-ggdb //;" Makefile
   sed -i "s/LD = gcc$/LD = $CC/;" Makefile
   sed -i "s/CXX = gcc$/CXX = $CC/;" Makefile
   make -j$J ENABLE_TCL=0 ENABLE_PLUGINS=0 ENABLE_READLINE=0 ENABLE_COVER=0 \
-            YOSYS_VER_STR="Yosys 0.7 (Apio build)" \
+            YOSYS_VER="0.7 (Apio build)" \
             LDLIBS="-static -lstdc++ -lm" \
             ABCMKARGS="CC=\"$CC\" CXX=\"$CXX\" ARCHFLAGS=\"$ABC_ARCHFLAGS -Wno-unused-but-set-variable\" \
-                       LIBS=\"-static -lm -ldl -lrt -pthread\" ABC_USE_NO_READLINE=1 "
+                       LIBS=\"-static -lm -ldl -pthread\" OPTFLAGS=\"-O\" ABC_USE_NO_READLINE=1"
 fi
 
 if [ $ARCH != "darwin" ]; then
