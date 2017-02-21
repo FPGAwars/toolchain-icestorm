3ca614feba2dd21575e271432e43f936b35c72da
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2017-02-21 13:19:46 +0100

    Travis test: linux_i686

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,34 +3,35 @@
 if [ $ARCH == "linux_x86_64" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3 libftdi1-dev # <- v1
+                          xdot pkg-config python3 libftdi-dev libftdi1-dev # <- v1
 fi
 
 if [ $ARCH == "linux_i686" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3 libftdi1-dev:i386
+                          xdot pkg-config python3 libftdi-dev libftdi1-dev:i386 \
+                          gcc-multilib g++-multilib
 fi
 
 if [ $ARCH == "linux_armv7l" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3 libftdi1-dev \
+                          xdot pkg-config python3 libftdi-dev libftdi1-dev \
                           gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
 fi
 
 if [ $ARCH == "linux_aarch64" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3 libftdi1-dev \
+                          xdot pkg-config python3 libftdi-dev libftdi1-dev \
                           gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
 fi
 
 if [ $ARCH == "windows" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
-                          xdot pkg-config python3 libftdi1-dev git-core \
-                          mingw-w64 mingw-w64-tools wine
+                          xdot pkg-config python3 libftdi-dev libftdi1-dev \
+                          git-core mingw-w64 mingw-w64-tools wine
 fi
 
 if [ $ARCH == "darwin" ]; then
