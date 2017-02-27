9c3f4c92cc38f397e5474611d7a91131f347feca
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2017-02-27 16:59:23 +0100

    Improve arachne-pnr performance

--- scripts/compile_arachnepnr.sh
+++ scripts/compile_arachnepnr.sh
@@ -22,7 +22,7 @@ if [ $ARCH == "darwin" ]; then
   make -j$J CXX="$CXX" LIBS="-lm" ICEBOX="../icestorm/icebox"
 else
   sed -i "s/bin\/arachne-pnr\ -d\ /\.\/bin\/arachne-pnr\ -d\ /;" Makefile
-  make -j$J CXX="$CXX" LIBS="-static -lm" ICEBOX="../icestorm/icebox"
+  make -j$J CXX="$CXX" LIBS="-static -static-libstdc++ -static-libgcc -lm" ICEBOX="../icestorm/icebox"
 fi
 
 if [ $ARCH != "darwin" ]; then
