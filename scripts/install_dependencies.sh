c593b23e84250b803e315273540af7cf2501f6c2
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2017-02-27 21:46:29 +0100

    Using gcc-5 for linux_(x86_64|i686) & windows_(amd64|x86)

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,15 +4,20 @@ if [ $ARCH == "linux_x86_64" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
                           xdot pkg-config python3 libftdi1-dev # <- v1
+                          gcc-5 g++-5
   sudo apt-get autoremove -y
+  sudo ln -sf /usr/bin/gcc-5 /usr/bin/gcc
+  sudo ln -sf /usr/bin/g++-5 /usr/bin/g++
 fi
 
 if [ $ARCH == "linux_i686" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
                           xdot pkg-config python3 libftdi1-dev \
-                          gcc-multilib g++-multilib
+                          gcc-5-multilib g++-5-multilib
   sudo apt-get autoremove -y
+  sudo ln -sf /usr/bin/gcc-5 /usr/bin/gcc
+  sudo ln -sf /usr/bin/g++-5 /usr/bin/g++
 fi
 
 if [ $ARCH == "linux_armv7l" ]; then
@@ -33,12 +38,28 @@ if [ $ARCH == "linux_aarch64" ]; then
   sudo apt-get autoremove -y
 fi
 
-if [ ${ARCH:0:7} == "windows" ]; then
+if [ $ARCH == "windows_x86" ]; then
   sudo apt-get install -y build-essential bison flex libreadline-dev \
                           gawk tcl-dev libffi-dev git mercurial graphviz \
                           xdot pkg-config python3 libftdi1-dev \
-                          mingw-w64 mingw-w64-tools wine
+                          gcc-5-mingw-w64 gc++-5-mingw-w64 \
+                          #mingw-w64 mingw-w64-tools \
+                          wine
   sudo apt-get autoremove -y
+  sudo ln -sf /usr/bin/i386-mingw32-gcc-5 /usr/bin/i386-mingw32-gcc
+  sudo ln -sf /usr/bin/i386-mingw32-g++-5 /usr/bin/i386-mingw32-g++
+fi
+
+if [ $ARCH == "windows_amd64" ]; then
+  sudo apt-get install -y build-essential bison flex libreadline-dev \
+                          gawk tcl-dev libffi-dev git mercurial graphviz \
+                          xdot pkg-config python3 libftdi1-dev \
+                          gcc-5-mingw-w64 gc++-5-mingw-w64 \
+                          #mingw-w64 mingw-w64-tools \
+                          wine
+  sudo apt-get autoremove -y
+  sudo ln -sf /usr/bin/x86-w64-mingw32-gcc-5 /usr/bin/x86-w64-mingw32-gcc
+  sudo ln -sf /usr/bin/x86-w64-mingw32-g++-5 /usr/bin/x86-w64-mingw32-g++
 fi
 
 if [ $ARCH == "darwin" ]; then
