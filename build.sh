ebe5a35743c58e9d6f7a556601db57badc7477bc
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2019-05-21 18:17:48 +0200

    Bump version to v1.11.1

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ export LC_ALL=C
 # Generate toolchain-icestorm-arch-ver.tar.gz from source code
 # sources: http://www.clifford.at/icestorm/
 
-VERSION=1.11.1-beta
+VERSION=1.11.1
 
 # -- Target architectures
 ARCH=$1
